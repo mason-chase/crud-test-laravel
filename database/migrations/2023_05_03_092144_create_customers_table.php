9cf6f6b6bb5d7248a7a98348264eb59ddcbd8690
Farhad Yozbashi <farhadysh@pop-os.localdomain>
2023-05-04 14:08:35 +0330

    Create Customer Migration

---
+++ database/migrations/2023_05_03_092144_create_customers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('date_of_birth');
+            $table->string('phone_number');
+            $table->string('email')->unique();
+            $table->bigInteger('bank_account_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+};
