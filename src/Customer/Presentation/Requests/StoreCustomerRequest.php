2c2b31cd039903171e7c3343045dda8953e200ff
Farshid Sohrabiyani <fsohrabi047@gmail.com>
2023-05-05 13:44:48 +0330

    added store customer request

---
+++ src/Customer/Presentation/Requests/StoreCustomerRequest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Src\Customer\Presentation\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Src\Customer\Application\Common\Interfaces\CustomerServiceInterface;
+use Src\Customer\Application\Rules\PhoneNumberRule;
+use Symfony\Component\HttpFoundation\Response;
+
+class StoreCustomerRequest extends FormRequest
+{
+
+    public function prepareForValidation()
+    {
+        $isExists = $this->customerService->checkExistenceByFields([
+            'first_name' => $this->input('first_name'),
+            'last_name' => $this->input('last_name'),
+            'date_of_birth' => $this->input('date_of_birth')
+        ]);
+
+        abort_if($isExists, Response::HTTP_UNPROCESSABLE_ENTITY, 'Customer exists!');
+    }
+
+    public function __construct(protected CustomerServiceInterface $customerService)
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name' => ['required', 'string', 'min:2'],
+            'last_name' => ['required', 'string', 'min:2'],
+            'date_of_birth' => ['required', 'date', 'date_format:Y-m-d'],
+            'phone_number' => ['required', 'string', 'min:2', new PhoneNumberRule('IR')],
+            'email' => ['required', 'email', Rule::unique('customers', 'email')],
+            'bank_account_number' => ['required']
+        ];
+    }
+}
