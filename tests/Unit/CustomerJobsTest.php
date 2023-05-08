07285398428b7b5c54c8d8e5ad34654e5e6b18de
Arash Rasoulzadeh <arashrasoulzadeh@gmail.com>
2023-05-08 18:26:24 +0330

    [ADD] customer jobs test

--- tests/Unit/CustomerJobsTest.php
+++ tests/Unit/CustomerJobsTest.php
@@ -7,14 +7,14 @@ use Database\Factories\CustomerFactory;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
-class ValidatorTest extends TestCase
+class CustomerJobsTest extends TestCase
 {
 	use DatabaseTransactions;
 
 	/**
 	 * A basic unit test example.
 	 */
-	public function test_example(): void
+	public function test_store_job(): void
 	{
 		$customerData = ( new ( CustomerFactory::class )() )->definition();
 		$customer     = dispatch_sync( new StoreJob( $customerData ) );
