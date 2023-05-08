eab52bb3e8639d17e5719511ce5aaddedf9a40b3
Arash Rasoulzadeh <arashrasoulzadeh@gmail.com>
2023-05-09 00:39:16 +0330

    [FIX] customer single unit

--- tests/Unit/CustomerJobsTest.php
+++ tests/Unit/CustomerJobsTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Jobs\Customer\IndexJob;
+use App\Jobs\Customer\SingleJob;
 use App\Jobs\Customer\StoreJob;
 use App\Models\Customer;
 use Database\Factories\CustomerFactory;
@@ -25,13 +26,23 @@ class CustomerJobsTest extends TestCase
 
 	public function test_index_job(): void
 	{
-		$total = rand(1,5);
-		for($i=0;$i<$total;$i++){
+		$total = rand( 1, 5 );
+		for ( $i = 0; $i < $total; $i++ )
+		{
 			$customerData = ( new ( CustomerFactory::class )() )->definition();
 			dispatch_sync( new StoreJob( $customerData ) );
 		}
 		$index = dispatch_sync( new IndexJob() );
-		$this->assertCount($total,$index);
+		$this->assertCount( $total, $index );
+	}
+
+	public function test_single_job(): void
+	{
+		$customerData = ( new ( CustomerFactory::class )() )->definition();
+		dispatch_sync( new StoreJob( $customerData ) );
+		$customer = Customer::first();
+		$single = dispatch_sync( new SingleJob($customer->id) );
+		$this->assertEquals($single,$customer);
 	}
 
 }
