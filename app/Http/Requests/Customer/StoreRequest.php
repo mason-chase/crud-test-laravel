a8c8be68507a936693b6fd7bc63f52161c87c193
Arash Rasoulzadeh <arashrasoulzadeh@gmail.com>
2023-05-08 19:37:49 +0330

    [FIX] request validator for customer store

---
+++ app/Http/Requests/Customer/StoreRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Customer;
+
+use App\Rules\PhoneNumber;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+	/**
+	 * Determine if the user is authorized to make this request.
+	 */
+	public function authorize(): bool
+	{
+		return true;
+	}
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+	 */
+	public function rules(): array
+	{
+		return [
+			'firstName'         => [ 'required', 'max:32' ],
+			'lastName'          => [ 'required', 'max:32' ],
+			'dateOfBirth'       => [ 'required', 'date' ],
+			'phoneNumber'       => [ 'required', new PhoneNumber() ],
+			'email'             => [ 'required', 'unique:customers,email' ],
+			'bankAccountNumber' => [ 'required' ],
+		];
+	}
+}
