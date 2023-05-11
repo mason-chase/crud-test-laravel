2384b16c62ddfc317529bd06156d5e4cc52b6f37
Khoshniat <khoshniat.hossein@gmail.com>
2023-05-11 12:39:30 +0330

    Implement CRUD functions

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,9 +2,59 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\CustomerRequest;
+use App\Http\Resources\CustomerResource;
+use App\Models\Customer;
+use Illuminate\Http\JsonResponse;
 
 class CustomerController extends Controller
 {
-    //
+    /**
+     * @return JsonResponse
+     */
+    public function view(): JsonResponse
+    {
+        return success_response(CustomerResource::collection(Customer::all())); //TODO:: Must be get data as pagination
+    }
+
+    /**
+     * @param Customer $customer
+     * @return JsonResponse
+     */
+    public function show(Customer $customer): JsonResponse
+    {
+        return success_response(new CustomerResource($customer));
+    }
+
+    /**
+     * @param CustomerRequest $request
+     * @return JsonResponse
+     */
+    public function store(CustomerRequest $request): JsonResponse
+    {
+        /** @var Customer $customer */
+        $customer = Customer::query()->create($request->all());
+
+        return success_response(new CustomerResource($customer));
+    }
+
+    /**
+     * @param CustomerRequest $request
+     * @param Customer $customer
+     * @return JsonResponse
+     */
+    public function update(CustomerRequest $request, Customer $customer): JsonResponse
+    {
+        return success_response(new CustomerResource($customer->update($request->all())));
+    }
+
+    /**
+     * @param Customer $customer
+     * @return JsonResponse
+     */
+    public function destroy(Customer $customer): JsonResponse
+    {
+        return success_response(new CustomerResource($customer->delete()));
+    }
+
 }
