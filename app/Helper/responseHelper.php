cf4cff7aae4296dba1c31f85d6d3f50f99727d7a
Khoshniat <khoshniat.hossein@gmail.com>
2023-05-11 12:04:48 +0330

    Create helper and register it

---
+++ app/Helper/responseHelper.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Contracts\Validation\Validator;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
+
+if (!function_exists("success_response")) {
+
+    function success_response($data = null, $message = 'messages.successfully_action', $statusCode = ResponseAlias::HTTP_OK): JsonResponse
+    {
+        return response()->json([
+            "success" => true,
+            "message" => __($message),
+            "data" => $data
+        ], $statusCode);
+
+    }
+}
+
+if (!function_exists("create_json_response")) {
+    function create_json_response($success, $message, $data, $statusCode): JsonResponse
+    {
+        return response()->json([
+            "success" => $success,
+            "message" => __($message),
+            "data" => $data
+        ], $statusCode);
+    }
+}
+
+if (!function_exists("error_response")) {
+    function error_response($data = null, $message = 'messages.unsuccessfully_action', $statusCode = ResponseAlias::HTTP_BAD_REQUEST): JsonResponse
+    {
+        return response()->json([
+            "success" => false,
+            "message" => __($message),
+            "data" => $data
+        ], $statusCode);
+    }
+}
+
+if (!function_exists("not_found_response")) {
+    function not_found_response($message = 'messages.not_found_model'): JsonResponse
+    {
+        return response()->json([
+            "success" => false,
+            "message" => __($message),
+            "data" => null
+        ], ResponseAlias::HTTP_NOT_FOUND);
+    }
+}
