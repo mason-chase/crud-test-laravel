72720660efed311edf4fff9a27c17b8f60d46571
Khoshniat <khoshniat.hossein@gmail.com>
2023-05-11 11:55:12 +0330

    Define Customer migration properties

--- database/migrations/2023_05_11_081517_create_customers_table.php
+++ database/migrations/2023_05_11_081517_create_customers_table.php
@@ -13,7 +13,15 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->timestamp('date_of_birth');
+            $table->string('phone_number', 17);
+            $table->string('email')->unique();
+            $table->string('bank_account_number');
             $table->timestamps();
+
+            $table->unique(['first_name', 'last_name', 'date_of_birth']);
         });
     }
 
