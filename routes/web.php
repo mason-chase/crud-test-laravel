dd55dd825a7b7205434432cce210cef90fc66a98
arash <arashmf71@gmail.com>
2023-05-13 15:32:15 +0330

    Add route docs

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('docs', \App\Http\Controllers\DocsController::class)
+    ->name('docs');
