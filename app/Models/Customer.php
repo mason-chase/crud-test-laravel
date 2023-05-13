ca9fda38af8cf40f72b3832623aba7352d231ddf
Khoshniat <khoshniat.hossein@gmail.com>
2023-05-13 11:43:06 +0330

    Check date_of_birth(use only 'Y-m-d' format)

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use DateTime;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -131,4 +132,12 @@ class Customer extends Model
      * @var string
      */
     private string $bank_account_number;
+
+    protected function dateOfBirth(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => date('Y-m-d', strtotime($value)),
+            set: fn ($value) => date('Y-m-d', strtotime($value)),
+        );
+    }
 }
